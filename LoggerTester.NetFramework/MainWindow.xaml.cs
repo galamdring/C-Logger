3223e31b939a07cb29a0664ae226eb9caeb06df8
Luke McKechnie <Luke.McKechnie@galamdring.com>
2019-11-27 06:15:01 -0600

    Added simple WPF application for an example of how to implement in .Net Framework

---
+++ LoggerTester.NetFramework/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoggerTester.NetFramework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MultiLogger Log;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoggerFactory.CreateLoggers(LogLevelEnum.Debug, new Type[] { typeof(MessageBoxLogger) });
+            Log = LoggerFactory.GetNewLogger("WPFFrameworkTest");
+        }
+
+        private void FireTest_Click(object sender, RoutedEventArgs e)
+        {
+            Log.Debug("This is a {0}","test");
+        }
+
+        private void FireTestEx_Click(object sender, RoutedEventArgs e)
+        {
+            Exception ex = new Exception("This is a test!");
+            Log.Debug(ex, "This is a test with an {0}!", "exception");
+            
+        }
+    }
+}
---
+++ LoggerTester.NetFramework/MessageBoxLogger.cs
@@ -0,0 +1,44 @@
+﻿using Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerTester.NetFramework
+{
+    class MessageBoxLogger : ILogger
+    {
+        public ILogger GetILogger(string name)
+        {
+            return this;
+        }
+
+        public void Log(LogLevelEnum level, string message, params object[] args)
+        {
+            //By firing the messagebox in a task, we don't block the ui of the main application. 
+            //In some instances this isn't the best way to go, as we would want the user to have to interact
+            //with the messagebox first.
+            //Also, the messageboxes seem to be waiting on each other, so you only get the second after closing the first
+            // and so on.
+            Task.Factory.StartNew
+                (
+                    () =>
+                    {
+                        System.Windows.MessageBox.Show(
+                            Utils.Format(message, args),
+                            level.ToString(),
+                            System.Windows.MessageBoxButton.OK,
+                            System.Windows.MessageBoxImage.Information,
+                            System.Windows.MessageBoxResult.OK,
+                            System.Windows.MessageBoxOptions.DefaultDesktopOnly);
+                    }
+                );
+        }
+
+        public void Log(LogLevelEnum level, Exception ex, string message, params object[] args)
+        {
+            Log(level, Utils.Format(Utils.Format("{0}{1}{2}", Utils.Format(ex), Environment.NewLine, message), args));
+        }
+    }
+}
